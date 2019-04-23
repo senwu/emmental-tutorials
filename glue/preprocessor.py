6240dc33e0b15b8f7842b422a1471fab41efc835
Sen Wu <senwu@cs.stanford.edu>
2019-04-23 11:06:56 -0700

    add tokenizer

--- glue/preprocessor.py
+++ glue/preprocessor.py
@@ -2,13 +2,106 @@ import os
 import codecs
 import logging
 
-from task_config import SPLIT_MAPPING, INDEX_MAPPING, SKIPPING_HEADER_MAPPING, LABEL_MAPPING
+from pytorch_pretrained_bert import BertTokenizer
+from pytorch_pretrained_bert.tokenization import BertTokenizer
+
+
+from task_config import (
+    SPLIT_MAPPING,
+    INDEX_MAPPING,
+    SKIPPING_HEADER_MAPPING,
+    LABEL_MAPPING,
+)
+
+try:
+    from IPython import get_ipython
+
+    if "IPKernelApp" not in get_ipython().config:
+        raise ImportError("console")
+except (AttributeError, ImportError):
+    from tqdm import tqdm
+else:
+    from tqdm import tqdm_notebook as tqdm
+
 
 logger = logging.getLogger(__name__)
 
-
-
 DELIMITER = "\t"
+
+
+def preprocessor(
+    data_dir,
+    task_name,
+    split,
+    bert_model="bert-large-uncased",
+    max_data_samples=None,
+    max_sequence_length=128,
+):
+
+    sentences, labels = parse_tsv(data_dir, task_name, split, max_data_samples)
+
+    do_lower_case = "uncased" in bert_model
+
+    tokenizer = BertTokenizer.from_pretrained(bert_model, do_lower_case=do_lower_case)
+
+    bert_token_ids = []
+    bert_token_masks = []
+    bert_token_segments = []
+
+    for sentence in sentences:
+        if len(sentence) not in [1, 2]:
+            logger.error("Sentence length doesn't match.")
+
+        # Tokenize sentences
+        tokenized_sentence = [tokenizer.tokenize(sent) for sent in sentence]
+        sent1_tokens = tokenized_sentence[0]
+        sent2_tokens = tokenized_sentence[1] if len(tokenized_sentence) == 2 else None
+
+        # One sentence case
+        if len(tokenized_sentence) == 1:
+            # Remove tokens that exceeds the max_sequence_length
+            if len(sent1_tokens) > max_sequence_length - 2:
+                # Account for [CLS] and [SEP] with "- 2"
+                sent1_tokens = sent1_tokens[: max_sequence_length - 2]
+        # Two sentences case
+        else:
+            # Remove tokens that exceeds the max_sequence_length
+            while True:
+                total_length = len(sent1_tokens) + len(sent2_tokens)
+                # Account for [CLS], [SEP], [SEP] with "- 3"
+                if total_length <= max_sequence_length - 3:
+                    break
+                if len(sent1_tokens) > len(sent2_tokens):
+                    sent1_tokens.pop()
+                else:
+                    sent2_tokens.pop()
+
+        # Convert to BERT manner
+        tokens = ["[CLS]"] + sent1_tokens + ["[SEP]"]
+        token_segments = [0] * len(tokens)
+
+        if sent2_tokens:
+            tokens += sent2_tokens + ["[SEP]"]
+            token_segments += [1] * (len(sent2_tokens) + 1)
+
+        token_ids = tokenizer.convert_tokens_to_ids(tokens)
+
+        # Generate mask where 1 for real tokens and 0 for padding tokens
+        token_masks = [1] * len(token_ids)
+
+        # Append padding
+        padding = [0] * (max_sequence_length - len(token_ids))
+
+        token_ids += padding
+        token_masks += padding
+        token_segments += padding
+
+        bert_token_ids.append(token_ids)
+        bert_token_masks.append(token_masks)
+        bert_token_segments.append(token_segments)
+
+    return bert_token_ids, bert_token_segments, bert_token_masks
+
 
 def parse_tsv(data_dir, task_name, split, max_data_samples=None):
     sentences = []
@@ -22,23 +115,24 @@ def parse_tsv(data_dir, task_name, split, max_data_samples=None):
 
         rows = list(enumerate(f))
 
-        # Truncate to max_data_samples        
+        # Truncate to max_data_samples
         if max_data_samples:
             rows = rows[:max_data_samples]
-        
+
+        # Calculate the max number of column
         max_cloumns = len(rows[0][1].strip().split(DELIMITER))
 
-        for idx, row in rows:
+        for idx, row in tqdm(rows):
             row = row.strip().split(DELIMITER)
-            
+
             if len(row) > max_cloumns:
                 logger.warning("Row has more columns than expected, skip...")
                 continue
-            
+
             sent1_idx, sent2_idx, label_idx = INDEX_MAPPING[task_name][split]
 
-            if  sent1_idx >= len(row) or sent2_idx >= len(row) or label_idx >= len(row):
-                logger.warning("Data column doesn't match data columns, skip...")
+            if sent1_idx >= len(row) or sent2_idx >= len(row) or label_idx >= len(row):
+                logger.warning("Data column doesn't match, skip...")
                 continue
 
             sent1 = row[sent1_idx]
@@ -46,9 +140,9 @@ def parse_tsv(data_dir, task_name, split, max_data_samples=None):
 
             if label_idx >= 0:
                 if LABEL_MAPPING[task_name] is not None:
-                    label = LABEL_MAPPING[task_name][row[label_idx]] 
+                    label = LABEL_MAPPING[task_name][row[label_idx]]
                 else:
-                    label = row[label_idx] 
+                    label = row[label_idx]
             else:
                 label = -1
 
@@ -57,7 +151,31 @@ def parse_tsv(data_dir, task_name, split, max_data_samples=None):
 
     return sentences, labels
 
+
 # Test purpose
 if __name__ == "__main__":
-    sentences, labels = parse_tsv("data", "RTE", "train", max_data_samples=5)
-    print(sentences, labels)
+    task_names = [
+        "CoLA",
+        "MNLI",
+        # "MNLI_matched",
+        # "MNLI_unmatched",
+        "MRPC",
+        "QNLI",
+        "QQP",
+        "RTE",
+        "SNLI",
+        "SST-2",
+        "STS-B",
+        "WNLI",
+    ]
+
+    splits = ["train", "dev", "test"]
+
+    for task_name in task_names:
+        for split in splits:
+            print(task_name, split)
+            # sentences, labels = parse_tsv("data", task_name, split, max_data_samples=2)
+            # print(sentences, labels)
+            # sentences, labels = parse_tsv("data", task_name, split)
+
+            preprocessor(data_dir, task_name, split, bert_model="bert-large-uncased", max_data_samples=2, max_sequence_length=20)

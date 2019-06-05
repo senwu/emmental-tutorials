b83aa6e163766b48efa28e0e3bf6dcc96596a330
Braden Hancock <hancock.braden@gmail.com>
2019-06-05 11:26:17 -0700

    Uncomment other tasks in submit.py

--- superglue/submit.py
+++ superglue/submit.py
@@ -7,17 +7,21 @@ import jsonlines
 from dataloaders import get_dataloaders
 import emmental
 from emmental.model import EmmentalModel
-from models.wic import build_model as build_model_wic
 from models.cb import build_model as build_model_cb
-from task_config import SuperGLUE_LABEL_INVERSE
+from models.copa import build_model as build_model_copa
+from models.multirc import build_model as build_model_multirc
+from models.rte import build_model as build_model_rte
+from models.wic import build_model as build_model_wic
+from models.wsc import build_model as build_model_wsc
+from task_config import SuperGLUE_LABEL_INVERSE, SuperGLUE_TASK_SPLIT_MAPPING
 
 build_model = {
     "CB": build_model_cb,
-    # "COPA": build_model_copa,
-    # "MultiRC": build_model_multirc,
-    # "RTE": build_model_rte,
+    "COPA": build_model_copa,
+    "MultiRC": build_model_multirc,
+    "RTE": build_model_rte,
     "WiC": build_model_wic,
-    # "WSC": build_model_wsc,
+    "WSC": build_model_wsc,
 }
 
 
@@ -30,6 +34,24 @@ WSC_MODEL = "/path/to/model"
 
 TASKS = ["CB", "COPA", "MultiRC", "RTE", "WiC", "WSC"]
 BERT_MODEL_NAME = "bert-large-cased"
+
+
+def format_multirc_preds(preds, dataloader, data_dir):
+    preds_formatted = []
+    task_name = "MultiRC"
+    split = "test"
+    # Make prediction map
+    
+    # Write answers into data
+    jsonl_path = os.path.join(
+        data_dir, task_name, SuperGLUE_TASK_SPLIT_MAPPING[task_name][split]
+    )
+    with jsonlines.open(jsonl_path) as reader:
+        for line in reader:
+            print(line)
+            import pdb; pdb.set_trace()
+    return preds_formatted
+
 
 @click.command()
 @click.option('--CB', default=CB_MODEL, help="Path to CB model")
@@ -47,8 +69,10 @@ def make_submission(name, split, data_dir, cb, copa, multirc, rte, wic, wsc):
         os.makedirs(os.path.dirname(submit_dir))
     
     for task_name, path in zip(TASKS, [cb, copa, multirc, rte, wic, wsc]):
-        if task_name not in ["WiC"]:
-            continue
+        # TEMP
+        # if task_name == "MultiRC":
+        #     continue
+        # # TEMP
         task = build_model[task_name](BERT_MODEL_NAME)
         model = EmmentalModel(name=f"SuperGLUE_{task_name}", tasks=[task])
         try:
@@ -69,18 +93,16 @@ def make_submission(name, split, data_dir, cb, copa, multirc, rte, wic, wsc):
             uid="uids",
         )
         # TEMP: Sanity check val performance
-        logging.info(model.score(dataloaders[0]))
+        # logging.info(model.score(dataloaders[0]))
         # TEMP
         
-        preds = model.predict(dataloaders[1], return_preds=True)["preds"][task_name]
-        logging.info(preds)
-
-        preds_formatted = []
-        for idx, y in enumerate(preds):
-            label = str(SuperGLUE_LABEL_INVERSE[task_name][y]).lower()
-            if task_name == "MultiRC":
-                raise NotImplementedError
-            else:
+        preds = model.predict(dataloaders[-1], return_preds=True)["preds"][task_name]
+        if task_name == "MultiRC":
+            preds_formatted = format_multirc_preds(preds, dataloaders[-1], data_dir)
+        else:
+            preds_formatted = []
+            for idx, y in enumerate(preds):
+                label = str(SuperGLUE_LABEL_INVERSE[task_name][y]).lower()
                 preds_formatted.append({"idx": idx, "label": label})
 
         filename = f'{task_name}.jsonl'

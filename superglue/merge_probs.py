d71a71a02326057b4f567ad5076670febb18680e
Braden Hancock <hancock.braden@gmail.com>
2019-06-12 14:47:35 -0700

    Add flags to merge_probs

--- superglue/merge_probs.py
+++ superglue/merge_probs.py
@@ -6,38 +6,39 @@ NOTE: This will not work for MultiRC
 
 from collections import defaultdict
 import os
+import subprocess
 
+import click
 import jsonlines
 import numpy as np
 
 from task_config import SuperGLUE_LABEL_INVERSE
 
-TASK_NAME = "CB"
+# ROOT = "/dfs/scratch1/bradenjh/emmental-tutorials/superglue/logs/CB/cb_5fold_slices"
+# ROOT = "/dfs/scratch1/vschen/superglue-models/COPA/from_SWAG/cross_val/2019_06_12"
 
-ROOT = "/dfs/scratch1/bradenjh/emmental-tutorials/superglue/logs/CB/test_cv_6/2019_06_12"
-IN_FILES = [
-    os.path.join(ROOT, "12_17_26/probs/CB_probs.jsonl"),
-    os.path.join(ROOT, "12_19_49/probs/CB_probs.jsonl"),
-    os.path.join(ROOT, "12_21_46/probs/CB_probs.jsonl"),
-    os.path.join(ROOT, "12_23_35/probs/CB_probs.jsonl"),
-    os.path.join(ROOT, "12_25_30/probs/CB_probs.jsonl"),
-]
+@click.command()
+@click.option('--task_name', required=True)
+@click.option('--root', required=True)
+def merge_probs(root, task_name):
+    INFILES = subprocess.check_output(f'find {root} -type f -name "*probs.jsonl"', shell=True).split()
+    OUTFILE = os.path.join(root, f"{task_name}.jsonl")
 
-OUTFILE = os.path.join(ROOT, f"{TASK_NAME}.jsonl")
+    preds_dict = defaultdict(list)
+    for probs_file in INFILES:
+        with jsonlines.open(probs_file, 'r') as reader:
+            for line in reader:
+                probs = np.array([float(p) for p in line["probs"][1:-1].split()])
+                preds_dict[line["idx"]].append(probs)
 
-preds_dict = defaultdict(list)
-for probs_file in IN_FILES:
-    with jsonlines.open(probs_file, 'r') as reader:
-        for line in reader:
-            probs = np.array([float(p) for p in line["probs"][1:-1].split()])
-            preds_dict[line["idx"]].append(probs)
+    preds_formatted = []
+    for idx, probs_list in preds_dict.items():
+        pred = np.argmax(np.array(probs_list).mean(axis=0)) + 1
+        label = str(SuperGLUE_LABEL_INVERSE[task_name][pred]).lower()
+        preds_formatted.append({"idx": idx, "label": label})
 
-preds_formatted = []
-for idx, probs_list in preds_dict.items():
-    pred = np.argmax(np.array(probs_list).mean(axis=0)) + 1
-    label = str(SuperGLUE_LABEL_INVERSE[TASK_NAME][pred]).lower()
-    preds_formatted.append({"idx": idx, "label": label})
+    with jsonlines.open(OUTFILE, 'w') as writer:
+        writer.write_all(preds_formatted)
 
-with jsonlines.open(OUTFILE, 'w') as writer:
-    writer.write_all(preds_formatted)
-
+if __name__ == "__main__":
+    merge_probs()

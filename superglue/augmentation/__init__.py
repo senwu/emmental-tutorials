ed0916f32d123cde9067cddc8f5cb4ef20537597
Vincent Chen <vincentschen@yahoo.com>
2019-06-12 19:12:52 -0700

    Remove incomplete COPA_Afs

--- superglue/augmentation/__init__.py
+++ superglue/augmentation/__init__.py
@@ -1,10 +1,10 @@
-from . import COPA_afs, WiC_afs
+from . import WiC_afs
 
 # sys.path.append("..")  # Adds higher directory to python modules path.
 
 augmentation_funcs = {
     # "CB": CB_afs.augmentation_funcs,
-    "COPA": COPA_afs.augmentation_funcs,
+    # "COPA": COPA_afs.augmentation_funcs,
     # "MultiRC": MultiRC_afs.augmentation_funcs,
     # "RTE": RTE_afs.augmentation_funcs,
     "WiC": WiC_afs.augmentation_funcs,

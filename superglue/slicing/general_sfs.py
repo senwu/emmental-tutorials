7df6d3e7d5d002b9bea8b6771e891cc64ccefd3e
vincentschen <vincentschen@yahoo.com>
2019-06-13 21:58:50 +0000

    Add more general SFs

--- superglue/slicing/general_sfs.py
+++ superglue/slicing/general_sfs.py
@@ -45,6 +45,49 @@ def slice_short_premise(example, thresh=10):
 def slice_long_premise(example, thresh=100):
     return len(example.sentence1.split()) > thresh
 
+@slicing_function(fields=["sentence1", "sentence2"])
+def slice_where(example):
+    sentences = example.sentence1 + example.sentence2
+    return "where" in sentences
+
+@slicing_function(fields=["sentence1", "sentence2"])
+def slice_who(example):
+    sentences = example.sentence1 + example.sentence2
+    return "who" in sentences
+
+@slicing_function(fields=["sentence1", "sentence2"])
+def slice_what(example):
+    sentences = example.sentence1 + example.sentence2
+    return "what" in sentences
+
+@slicing_function(fields=["sentence1", "sentence2"])
+def slice_when(example):
+    sentences = example.sentence1 + example.sentence2
+    return "when" in sentences
+
+@slicing_function(fields=["sentence1", "sentence2"])
+def slice_and(example):
+    sentences = example.sentence1 + example.sentence2
+    return "and" in sentences
+
+@slicing_function(fields=["sentence1", "sentence2"])
+def slice_but(example):
+    sentences = example.sentence1 + example.sentence2
+    return "but" in sentences
+
+@slicing_function(fields=["sentence1", "sentence2"])
+def slice_or(example):
+    sentences = example.sentence1 + example.sentence2
+    return "or" in sentences
+
+@slicing_function(fields=["sentence1", "sentence2"])
+def slice_multiple_articles(example):
+    sentences = example.sentence1 + example.sentence2
+    multiple_indefinite = sum([int(x == "a") for x in sentences.split()]) > 1 \
+        or sum([int(x == "an") for x in sentences.split()]) > 1
+    multiple_definite = sum([int(x == "the") for x in sentences.split()]) > 1
+    return multiple_indefinite or multiple_definite
+
 slices = [
     slice_temporal_preposition,
     slice_possessive_preposition,
@@ -53,7 +96,15 @@ slices = [
     slice_short_hypothesis,
     slice_long_hypothesis,
     slice_short_premise,
-    slice_long_premise
+    slice_long_premise,
+    slice_where,
+    slice_who,
+    slice_what,
+    slice_when,
+    slice_and,
+    slice_or,
+    slice_but,
+    slice_multiple_articles
 ]
 
 slice_func_dict = {slice.__name__: slice for slice in slices}

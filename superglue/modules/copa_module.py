d4d8281ad5a86ac9a3d6c4e9a5acc7a4a2aab84d
Sen Wu <senwu@cs.stanford.edu>
2019-06-20 20:57:01 +0000

    update copa

--- superglue/modules/copa_module.py
+++ superglue/modules/copa_module.py
@@ -1,18 +1,22 @@
 import torch
 from torch import nn
 
-
-class ChoiceModule(nn.Module):
+class PreModule(nn.Module):
     def __init__(self, n_choices=2):
         super().__init__()
 
         self.n_choices = n_choices
 
-    def forward(self, immediate_ouput_dict):
-        logits = []
-        for i in range(self.n_choices):
-            logits.append(immediate_ouput_dict[f"choice{str(i)}rep"][0])
+    def forward(self, immediate_output_dict):
+        token_ids = torch.cat([immediate_output_dict["_input_"][f"token{i+1}_ids"] for i in range(self.n_choices)], dim=-1)
+        token_masks = torch.cat([immediate_output_dict["_input_"][f"token{i+1}_masks"] for i in range(self.n_choices)], dim=-1)
+        token_segments = torch.cat([immediate_output_dict["_input_"][f"token{i+1}_segments"] for i in range(self.n_choices)], dim=-1)
 
-        logits = torch.cat(logits, dim=1)
+        batch_size, seq_length = token_ids.size()
 
-        return logits
+        token_ids = token_ids.view(batch_size * self.n_choices, -1)
+        token_masks = token_masks.view(batch_size * self.n_choices, -1)
+        token_segments = token_segments.view(batch_size * self.n_choices, -1)
+
+        return token_ids, token_masks, token_segments
+
